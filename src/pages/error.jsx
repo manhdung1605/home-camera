5466ffad364e83dad16def52cb539ac00cf19b28
Phan Dinh Duy <duypd7@viettel.com.vn>
2022-10-12 10:54:08 +0700

    error

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,370 +1,10 @@
-import { Button, Col, DatePicker, Drawer, Form, Input, Row, Select, Space, Typography, Table } from "antd";
+import { Button, Col, DatePicker, Drawer, Form, Input, Row, Select, Space, Typography, Table, Modal, Popconfirm } from "antd";
 import './error.css'
 import { useState } from "react";
+import { data } from "../fake-data/error"
 const { Option } = Select;
 const Error = () => {
     const { Search } = Input;
-    const [rowPerPage, setRowPerPage] = useState(10);
-    const [searchedDevice, setSearchedDevice] = useState('')
-    const handleChange = event => {
-        setRowPerPage(event.target.value)
-    }
-    const data = [
-        {
-            stt: 1,
-            deviceId: 'CNME000000001',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F5',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Son',
-            key: '1'
-        },
-        {
-            stt: 2,
-            deviceId: 'CNME000000024',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F6',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Phong',
-            key: '2'
-        },
-        {
-            stt: 3,
-            deviceId: 'CNME000000100',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F7',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Hiến',
-            key: '3'
-        },
-        {
-            stt: 4,
-            deviceId: 'CNME000000001',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F5',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Son',
-            key: '4'
-        },
-        {
-            stt: 5,
-            deviceId: 'CNME000000024',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F6',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Phong',
-            key: '5'
-        },
-        {
-            stt: 6,
-            deviceId: 'CNME000000100',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F7',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Hiến',
-            key: '6'
-        },
-        {
-            stt: 7,
-            deviceId: 'CNME000000001',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F5',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Son',
-            key: '7'
-        },
-        {
-            stt: 8,
-            deviceId: 'CNME000000024',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F6',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Phong',
-            key: '8'
-        },
-        {
-            stt: 9,
-            deviceId: 'CNME000000100',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F7',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Hiến',
-            key: '9'
-        },
-        {
-            stt: 10,
-            deviceId: 'CNME000000001',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F5',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Son',
-            key: '10'
-        },
-        {
-            stt: 11,
-            deviceId: 'CNME000000024',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F6',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Phong',
-            key: '11'
-        },
-        {
-            stt: 12,
-            deviceId: 'CNME000000100',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F7',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Hiến',
-            key: '12'
-        },
-        {
-            stt: 13,
-            deviceId: 'CNME000000001',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F5',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Son',
-            key: '13'
-        },
-        {
-            stt: 14,
-            deviceId: 'CNME000000024',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F6',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Phong',
-            key: '14'
-        },
-        {
-            stt: 15,
-            deviceId: 'CNME000000100',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F7',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Hiến',
-            key: '15'
-        },
-        {
-            stt: 16,
-            deviceId: 'CNME000000001',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F5',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Son',
-            key: '16'
-        },
-        {
-            stt: 17,
-            deviceId: 'CNME000000024',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F6',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Phong',
-            key: '17'
-        },
-        {
-            stt: 18,
-            deviceId: 'CNME000000100',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F7',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Hiến',
-            key: '18'
-        },
-        {
-            stt: 19,
-            deviceId: 'CNME000000001',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F5',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Son',
-            key: '19'
-        },
-        {
-            stt: 20,
-            deviceId: 'CNME000000024',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F6',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Phong',
-            key: '20'
-        },
-        {
-            stt: 21,
-            deviceId: 'CNME000000100',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F7',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Hiến',
-            key: '21'
-        },
-        {
-            stt: 22,
-            deviceId: 'CNME000000001',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F5',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Son',
-            key: '22'
-        },
-        {
-            stt: 23,
-            deviceId: 'CNME000000024',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F6',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Phong',
-            key: '23'
-        },
-        {
-            stt: 24,
-            deviceId: 'CNME000000100',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F7',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Hiến',
-            key: '24'
-        },
-        {
-            stt: 25,
-            deviceId: 'CNME000000001',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F5',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Son',
-            key: '25'
-        },
-        {
-            stt: 26,
-            deviceId: 'CNME000000024',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F6',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Phong',
-            key: '26'
-        },
-        {
-            stt: 27,
-            deviceId: 'CNME000000100',
-            deviceType: 'HC22',
-            mac: '24:0B:2A:72:20:F7',
-            errorId: 'FFFFFFFF',
-            errorDetail: 'Giải mã chi tiết lỗi tại đây',
-            errorDate: '30/8/2022 17:30:05',
-            custommer: 'Hiến',
-            key: '27'
-        },
-    ]
-
-    const columns = [
-        {
-            title: '#',
-            dataIndex: 'stt',
-            key: 'stt'
-        },
-        {
-            title: 'Mã thiết bị',
-            dataIndex: 'deviceId',
-            key: 'deviceId',
-            filteredValue: [searchedDevice],
-            onFilter: (value, record) => {
-                return String(record.deviceId).toLowerCase().includes(value.toLowerCase());
-            }
-        },
-        {
-            title: 'Loại thiết bị',
-            dataIndex: 'deviceType',
-            key: 'deviceType'
-        },
-        {
-            title: 'Địa chỉ MAC',
-            dataIndex: 'mac',
-            key: 'mac'
-        }, {
-            title: 'Mã lỗi',
-            dataIndex: 'errorId',
-            key: 'errorId'
-        },
-        {
-            title: 'Chi tiết lỗi',
-            dataIndex: 'errorDetail',
-            key: 'errorDetail'
-        },
-        {
-            title: 'Ngày ghi nhận',
-            dataIndex: 'errorDate',
-            key: 'errorDate'
-        },
-        {
-            title: 'Khách hàng',
-            dataIndex: 'custommer',
-            key: 'custommer'
-        },
-        {
-            title: 'Hành động',
-            dataIndex: 'action',
-            key: 'action',
-            render: (_, record) => (
-                <Space size="middle">
-                    <Button type="primary">Sửa</Button>
-                    <Button type="danger">Xóa</Button>
-                </Space>
-            )
-        }
-    ]
     const [open, setOpen] = useState(false);
     const [idDevice, setIdDevice] = useState('');
     const [typeDevice, setTypeDevice] = useState('');
@@ -373,25 +13,218 @@ const Error = () => {
     const [dateTime, setDateTime] = useState('');
     const [customer, setCustomer] = useState('');
     const [description, setDescription] = useState('');
+    const [rowPerPage, setRowPerPage] = useState(10);
+    const [searchedDevice, setSearchedDevice] = useState('')
+    const [editRowKey, setEditRowKey] = useState('')
+
+    const [form] = Form.useForm()
+    const [form2] = Form.useForm()
+
+    const handleChange = event => {
+        setRowPerPage(event.target.value)
+    }
+
+    const columns = [
+        {
+            title: 'STT',
+            dataIndex: 'stt',
+            editable: true,
+            key: 'stt'
+        },
+        {
+            title: 'Mã thiết bị',
+            dataIndex: 'idDevice',
+            key: 'idDevice',
+        editable: true,
+            filteredValue: [searchedDevice],
+            onFilter: (value, record) => {
+                return String(record.idDevice).toLowerCase().includes(value.toLowerCase());
+            }
+        },
+        {
+            title: 'Loại thiết bị',
+            dataIndex: 'typeDevice',
+            key: 'typeDevice',
+            editable: true,
+        },
+        {
+            title: 'Địa chỉ MAC',
+            dataIndex: 'mac',
+            key: 'mac',
+            editable: true,
+        }, {
+            title: 'Mã lỗi',
+            dataIndex: 'idError',
+            editable: true,
+            key: 'idError'
+        },
+        {
+            title: 'Chi tiết lỗi',
+            dataIndex: 'description',
+            editable: true,
+            key: 'description'
+        },
+        {
+            title: 'Ngày ghi nhận',
+            dataIndex: 'dateTime',
+            editable: true,
+            key: 'dateTime'
+        },
+        {
+            title: 'Khách hàng',
+            dataIndex: 'custommer',
+            editable: true,
+            key: 'custommer'
+        },
+        {
+            title: 'Hành động',
+            dataIndex: 'action',
+            key: 'action',
+            render: (text, record, index) => {
+                const editable = isEditing(record)
+                return <Space>
+                    <Popconfirm
+                        title="Bạn có chắc chắn muốn xóa ?"
+                        onConfirm={() => console.log('delete')}
+                    >
+                        <Button danger type="primary" disabled={editable} >
+                            Xóa
+                        </Button>
+                    </Popconfirm>
+                    {editable ? (
+                        <span>
+                            <Space size="middle" >
+                                <Button
+                                    onClick={e => save(record.key)}
+                                    type="primary"
+                                    style={{ marginRight: 8 }}
+                                >
+                                    Lưu
+                                </Button>
+                                <Popconfirm title="Bạn chắc chắn muốn hủy ?" onConfirm={cancel}>
+                                    <Button>Hủy</Button>
+                                </Popconfirm>
+                            </Space>
+                        </span>
+                    ) : (
+                        <Button onClick={() => edit(record)} type="primary">
+                            Sửa
+                        </Button>
+                    )}
+
+                </Space >
+            }
+        }
+    ]
+
+    const isEditing = (record) => {
+        return record.key === editRowKey;
+    }
+    const cancel = () => {
+        setEditRowKey('');
+
+    }
+    const save = () => {
+
+    }
+    const edit = (record) => {
+        form2.setFieldsValue({
+            idDevice: "",
+            typeDevice: "",
+            mac: "",
+            idError: "",
+            dateTime: "",
+            customer: "",
+            description: "",
+            ...record,
+        });
+        setEditRowKey(record.key);
+    }
 
     const showDrawer = () => {
+        form.setFieldsValue({
+            idDevice: "",
+            typeDevice: "",
+            mac: "",
+            idError: "",
+            dateTime: "",
+            customer: "",
+            description: ""
+        })
         setOpen(true);
+        console.log("showDrawer");
     };
 
+    const mergedColumns = columns.map((col) => {
+        if (!col.editTable) {
+            return col;
+        } 
+        return {
+            ...col,
+            onCell: (record) => ({
+                record,
+                dataIndex: col.dataIndex,
+                title: col.title,
+                editing: isEditing(record),
+            }),
+        };
+    });
+
+    const EditableCell = ({
+        editing,
+        dataIndex,
+        title,
+        record,
+        children,
+        ...restProps
+    }) => {
+        const input = <Input />;
+        return (
+            <td {...restProps}>
+                {editing ? (
+                    <Form.Item
+                        name={dataIndex}
+                        style={{ margin: 0 }}
+                        rules={[
+                            {
+                                required: true,
+                                message: `Nhập ${title}`,
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                ) : (
+                    children
+                )}
+            </td>
+        )
+    }
     const onClose = () => {
+        form.setFieldsValue({
+            idDevice: "",
+            typeDevice: "",
+            mac: "",
+            idError: "",
+            dateTime: "",
+            customer: "",
+            description: ""
+        })
         setOpen(false);
     };
 
-    const handleSubmit = () => {
-        console.log(idDevice)
+    const handleSubmit = (e) => {
+        console.log(e.idDevice)
         console.log('submit')
         setOpen(false)
     }
 
+
+
     return <div>
         <Typography style={{ margin: "40px" }}>BÁO CÁO LỖI</Typography>
         <Drawer
-            title="Thêm lỗi"
+            title=""
             width={720}
             onClose={onClose}
             open={open}
@@ -399,8 +232,8 @@ const Error = () => {
                 paddingBottom: 80,
             }}
         >
-            <Form layout="vertical" hideRequiredMark onFinish={handleSubmit}>
-              
+            <Form form={form} layout="vertical" hideRequiredMark onFinish={handleSubmit}>
+
                 <Row gutter={16}>
                     <Col span={12}>
                         <Form.Item
@@ -521,9 +354,9 @@ const Error = () => {
                     </Col>
                 </Row>
                 <Space>
-                    <Button onClick={onClose}>Hủy bỏ</Button>
+                    <Button onClick={onClose} type="danger">Hủy bỏ</Button>
                     <Button htmlType="submit" type="primary">
-                        Thêm mới
+                        Xác nhận
                     </Button>
                 </Space>
             </Form>
@@ -547,16 +380,22 @@ const Error = () => {
             />
             <Button type="primary" onClick={showDrawer}>Tạo mới</Button>
         </div>
-        <Table
-            dataSource={data}
-            columns={columns}
-            bordered
-            pagination={{
-                pageSize: rowPerPage
-            }}
-        >
-
-        </Table>
+        <Form form={form2} component={false} >
+            <Table
+                components={{
+                    body: {
+                        cell: EditableCell,
+                    },
+                }}
+                dataSource={data}
+                columns={mergedColumns}            
+                bordered
+                pagination={{
+                    pageSize: rowPerPage
+                }}
+            >
+            </Table>
+        </Form>
     </div>
 }
 
